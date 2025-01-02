6fef4ab115146bb79033a4bf94e280e0280014b4
karanyadavv <karanyadav729@gmail.com>
2025-01-02 22:16:08 +0530

    navbar

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const IntroSection = () => {
   return (
-    <div className="flex flex-col items-center space-y-8">
+    <div className="flex flex-col items-center space-y-8 mb-24">
       <button className="btn-outline">
         <p className="btn-outline-text">Beta 1.5 available now</p>
       </button>
       <p className="text-[40px] md:text-[58px] text-white font-medium font-archivo leading-tight md:tracking-normal tracking-tight text-wrap">
         Accelerating sales & growth with our AI solutions.
       </p>
-      <p className="font-inter text-[#aaaaaa] font-[28px] text-wrap mt-4 leading-relaxed text-justify">
+      <p className="font-inter text-[#aaaaaa] font-[28px] text-wrap mt-4 leading-relaxed text-center">
         Our AI-powered tools are designed to analyze vast amounts of data,
         uncover hidden patterns, and provide actionable insights that drive
         smarter decision.
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,17 @@ import logoImg from "../assets/logo.png";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-center items-center text-white space-x-52 mb-10 font-inter">
-      <div>
+    <div className="flex justify-center items-center text-white space-x-52 font-inter mb-12 md:mb-20">
+      <div className="w-[150px] md:w-[170px]">
         <img src={logoImg} alt="Logo" height={170} width={170} />
       </div>
-      <div className="flex justify-center items-center space-x-4">
+      <div className="hidden md:flex justify-center items-center space-x-4">
         <span>All pages</span>
         <span>Features</span>
         <span>Pricing</span>
         <span>About Us</span>
       </div>
-      <div>
+      <div className="hidden md:block">
         <button className="btn-outline">
           <p className="btn-outline-text text-[16px] px-6 py-4">
             Get the Template
